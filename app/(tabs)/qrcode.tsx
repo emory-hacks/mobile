44b362059b1f33ef52275a51cdb2cac6acd2d890
TAEEUN KIM <taeeunk1208@gmail.com>
2026-03-28 16:17:33 -0400

    set up tabs

---
+++ app/(tabs)/qrcode.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function QRCodeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>QR Code Screen</Text>
+    </View>
+  );
+}
