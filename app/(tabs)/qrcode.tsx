7171a4abe967826a6e878ea254c5f2f13b0e85af
Tofudog <leodefarias25@gmail.com>
2026-04-08 21:14:07 -0400

    Upload + display temporary code

--- app/(tabs)/qrcode.tsx
+++ app/(tabs)/qrcode.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Image, View, useWindowDimensions } from "react-native";
 
 export default function QRCodeScreen() {
+  const { width, height } = useWindowDimensions();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>QR Code Screen</Text>
+      <Image
+        source={require("../../assets/images/sample_qr_code.png")}
+        style={{
+          width: 0.5 * width,
+          height: 0.3 * height,
+        }}
+      />
     </View>
   );
 }
