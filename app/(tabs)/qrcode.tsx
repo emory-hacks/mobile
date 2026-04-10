1327fa5e9477f1adf98088fc4f382f7672084af2
Tofudog <leodefarias25@gmail.com>
2026-04-10 16:51:34 -0400

    Fix image for user side

--- app/(tabs)/qrcode.tsx
+++ app/(tabs)/qrcode.tsx
@@ -1,16 +1,37 @@
-import { Image, View, useWindowDimensions } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  useColorScheme,
+  useWindowDimensions,
+} from "react-native";
 
 export default function QRCodeScreen() {
   const { width, height } = useWindowDimensions();
+  let colorScheme = useColorScheme();
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Image
-        source={require("../../assets/images/sample_qr_code.png")}
-        style={{
-          width: 0.5 * width,
-          height: 0.3 * height,
-        }}
-      />
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: "center", padding: 45 }}>
+        <Text
+          style={{
+            color: colorScheme === "dark" ? "white" : "black",
+            fontSize: 25,
+          }}
+        >
+          Show your QR
+        </Text>
+      </View>
+      <View
+        style={{ flex: 0.8, alignItems: "center", justifyContent: "center" }}
+      >
+        <Image
+          source={require("../../assets/images/sample_qr_code.png")}
+          style={{
+            width: 0.55 * width,
+            height: 0.3 * height,
+          }}
+        />
+      </View>
     </View>
   );
 }
