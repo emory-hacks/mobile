f53a9db3f913910308c74411c693c2cc4bb4fb81
yizewang2006 <99563009+yizewang2006@users.noreply.github.com>
2026-07-03 13:55:08 -0400

    Add QR code
    * Echo reject file
    
    * Upload + display temporary code
    
    * Fix image for user side
    
    * QR Code Page Update
    
    * Update qrcode.tsx
    
    * Deleted Files & Made some changes to the code.
    
    * Update qrcode.tsx
    
    ---------
    
    Co-authored-by: Tofudog <leodefarias25@gmail.com>

--- app/(tabs)/qrcode.tsx
+++ app/(tabs)/qrcode.tsx
@@ -1,9 +1,65 @@
-import { Text, View } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import * as Brightness from "expo-brightness";
+import { useCallback, useRef } from "react";
+import { Text, View, useColorScheme } from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 export default function QRCodeScreen() {
+  let colorScheme = useColorScheme();
+  const originalBrightness = useRef<number | null>(null);
+
+  useFocusEffect(
+    useCallback(() => {
+      let active = true;
+      (async () => {
+        if (active) {
+          try {
+            // Save the original brightness & set to MAX
+            originalBrightness.current = await Brightness.getBrightnessAsync();
+            await Brightness.setBrightnessAsync(1);
+          } catch {
+            // brightness control not available
+          }
+        }
+      })();
+
+      return () => {
+        active = false;
+        if (originalBrightness.current !== null) {
+          try {
+            Brightness.setBrightnessAsync(originalBrightness.current);
+          } catch {
+            // brightness control not available
+          }
+        }
+      };
+    }, [])
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>QR Code Screen</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: "center", paddingTop: 100, paddingBottom: 20 }}>
+        <Text
+          style={{
+            color: colorScheme === "dark" ? "white" : "black",
+            fontSize: 25,
+          }}
+        > {/* (This is a placeholder, we need to figure out how to generate the QR code for each user) */}
+          Your Unique QR Code
+        </Text>
+      </View>
+      <View
+        style={{ flex: 0.8, alignItems: "center", justifyContent: "center" }}
+      >
+        <View style={{ backgroundColor: "white", padding: 10, borderRadius: 10 }}>
+          <QRCode
+            value="https://expo.dev"
+            size={200}
+            color="black"
+            backgroundColor="white"
+          />
+        </View>
+      </View>
     </View>
   );
 }
